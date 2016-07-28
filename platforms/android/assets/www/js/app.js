48eb18a6f41e0e23467c9dd47ce0c51f619fab67
salarelv <taavi@root.ee>
2016-07-28 12:07:53 +0300

    AZ, Groups, POIInfo, project from settings

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'jett.ionic.filter.bar'])
+angular.module('starter', ['ionic', 'app.controllers', 'app.services', 'jett.ionic.filter.bar', 'wf.api'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,9 +22,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   });
 })
-.run(['wfangular3d', function(wayfinder) {
+.run(['wfangular3d', 'wf.api.config', '$localStorage', function(wayfinder, wfConfig, storage) {
+  if(!storage.project)
+    storage.project = "af59cda079cc916bee6557134c110e86";
   wayfinder.options.assetsLocation = 'http://static.3dwayfinder.com/shared/';
-  wayfinder.open("af59cda079cc916bee6557134c110e86");
+  wfConfig.location = "http://api.3dwayfinder.com";
+  wfConfig.project = storage.project
+  wayfinder.open(storage.project);
 }])
 
 .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -39,10 +43,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     .state('tab', {
       url: '/tab',
       abstract: true,
-      templateUrl: 'templates/tabs.html'
+      templateUrl: 'templates/tabs.html',
+      controller: 'TabsController'
     })
     .state('tab.map', {
       url: '/map',
+      preload: true,
       views: {
         'tab-map': {
           templateUrl: 'templates/tab-map.html',
@@ -50,31 +56,49 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         }
       }
     })
-    .state('tab.chats', {
-        url: '/chats',
-        views: {
-          'tab-chats': {
-            templateUrl: 'templates/tab-chats.html',
-            controller: 'ChatsCtrl'
-          }
-        }
-      })
-      .state('tab.chat-detail', {
-        url: '/chats/:chatId',
-        views: {
-          'tab-chats': {
-            templateUrl: 'templates/chat-detail.html',
-            controller: 'ChatDetailCtrl'
-          }
-        }
-      })
-
-    .state('tab.account', {
-      url: '/account',
+    .state('tab.topics', {
+      url: '/topics',
+      preload: true,
       views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
+        'tab-topics': {
+          templateUrl: 'templates/tab-topics.html',
+          controller: 'TopicsController'
+        }
+      }
+    })
+    .state('tab.topic', {
+      url: '/topic/:id',
+      views: {
+        'tab-topics': {
+          templateUrl: 'templates/tab-topics-pois.html',
+          controller: 'TopicPOIsController'
+        }
+      }
+    })
+    .state('tab.az', {
+      url: '/az',
+      views: {
+        'tab-az': {
+          templateUrl: 'templates/tab-az.html',
+          controller: 'AZController'
+        }
+      }
+    })
+    .state('tab.poi', {
+      url: '/poi/:id',
+      views: {
+        'tab-az': {
+          templateUrl: 'templates/tab-poi.html',
+          controller: 'POIController'
+        }
+      }
+    })
+    .state('tab.settings', {
+      url: '/settings',
+      views: {
+        'tab-az': {
+          templateUrl: 'templates/tab-settings.html',
+          controller: 'SettingsController'
         }
       }
     });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,24 @@
-angular.module('starter.controllers', ['wfangular'])
+angular.module('app.controllers', ['wfangular', 'ngCordova'])
 .filter('wfSearch', function() {
   return ;
 })
-.controller('MapController', function($scope, wfangular3d, $ionicFilterBar) {
+.controller('MapController', function($scope, wfangular3d, $ionicFilterBar, $stateParams) {
+  var _poi = $stateParams.poi;
   var filterBarInstance;
   $scope.items = [];
+
+  console.log("poi", _poi);
+  if(_poi){
+
+    var poi = wfangular3d.pois[_poi];
+    if(poi)
+      wfangular3d.showPath(poi.getNode(), poi);
+  }
+
   $scope.showFilterBar = function () {
    filterBarInstance = $ionicFilterBar.show({
      items: $scope.items,
      filter: function(input, keyword) {
-       console.log("wfSearch", wfangular3d.search.search(keyword), keyword);
        return wfangular3d.search.search(keyword);
      },
      update: function (filteredItems, filterText) {
@@ -26,29 +35,128 @@ angular.module('starter.controllers', ['wfangular'])
       wfangular3d.showPath(poi.getNode(), poi);
    }
  };
+})
+.controller('TabsController', ['$scope', 'wfangular3d', '$stateParams', '$rootScope', '$cordovaSplashscreen', '$ionicPlatform', '$cordovaBeacon', function($scope, wayfinder, $stateParams, $rootScope, $cordovaSplashscreen, $cordovaBeacon,  $ionicPlatform) {
+  $scope.$on("$ionicView.afterEnter", function(event, data){
+     // Pause Wayfinder when leaving the map tab
+     if(wayfinder && wayfinder.engine){
+        if(data.stateId == "tab.map"){
+          wayfinder.engine.run();
+        }  
+        else
+          wayfinder.engine.pause();
+     }
+  });
+
+  function createBeacon() {
+
+    var uuid = 'EBEFD083-70A2-47C8-9837-E7B5634DF524'; // mandatory
+    var identifier = 'beaconAtTheMacBooks'; // mandatory
+    var minor = 4; // optional, defaults to wildcard if left empty
+    var major = 1; // optional, defaults to wildcard if left empty
+
+    // throws an error if the parameters are not valid
+    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+    return beaconRegion;   
+  } 
+
+  ionic.Platform.ready(function(){
+    var beaconRegion = createBeacon();
+    var delegate = new cordova.plugins.locationManager.Delegate();
+    
+    delegate.didDetermineStateForRegion = function (pluginResult) {
+      console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+    };
+
+    delegate.didStartMonitoringForRegion = function (pluginResult) {
+        console.log('didStartMonitoringForRegion:'+ JSON.stringify(pluginResult));
+    };
+
+    delegate.didRangeBeaconsInRegion = function (pluginResult) {
+        console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+    };
+
+    delegate.didEnterRegion = function (result) {
+        console.log("didEnterRegion "+JSON.stringify(result))
+        delegate.requestStateForRegion(beaconRegion)
+    };
+
+    delegate.didExitRegion = function (result) {
+      console.log("didExitRegion")
+    };
+
+    cordova.plugins.locationManager.isBluetoothEnabled()
+    .then(function(isEnabled){
+      if(!isEnabled){
+        cordova.plugins.locationManager.enableBluetooth();        
+      }
+    })
+    .fail(function(e) { console.error(e); })
+    .done();
+
+    cordova.plugins.locationManager.setDelegate(delegate);
+
+    // required in iOS 8+
+    //cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
+    cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+    cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
+        .fail(console.error)
+        .done();
+
+
+  });
+
+  $scope.$on("wf.show.path", function(event, poi){
+    console.log("wf.show.path", poi.getNode(), poi)
+    wayfinder.showPath(poi.getNode(), poi);
+  });
+
+  $scope.$on("wf.data.loaded", function(event, poi){
+    $cordovaSplashscreen.hide();
+  });
+}])
+.controller('TopicsController', ['$scope', 'wfangular3d', '$state', function($scope, wayfinder, $state) {
+  $scope.topics = wayfinder.getPOIGroups();
+
+  $scope.go = function(){
+    console.log("go")
+    $state.go("tab.topic", {id: 1});
+  }
+}])
+.controller('AZController', function($scope, wfangular3d) {
+  var language = wfangular3d.getLanguage();
+  $scope.pois = wfangular3d.getPOIs();
+  $scope.poiKeys = Object.keys($scope.pois).sort(function(a,b){
+    if($scope.pois[a].getName(language) && $scope.pois[b].getName(language))
+        return $scope.pois[a].getName(language).toLowerCase().trim().localeCompare($scope.pois[b].getName(language).toLowerCase().trim());
+  });
 
 })
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('POIController', function($scope, $rootScope, $stateParams, wfangular3d, $state) {
+  var id = $stateParams.id;
+  $scope.poi = wfangular3d.pois[id];
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.showPath = function(){
+    $rootScope.$broadcast("wf.show.path", $scope.poi);
+    $state.go("tab.map");
+  }
+
 })
+.controller('TopicPOIsController', function($scope, $stateParams, wfangular3d) {
+  var id = $stateParams.id;
+  $scope.group = wfangular3d.getPOIGroups()[id];
+  console.log("TopicPOIsController", $stateParams.id, $scope.pois)
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
+.controller('SettingsController', ['$scope', '$localStorage', '$window', function($scope, storage, $window) {
+  $scope.form = {project: storage.project};
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  $scope.onSave = function(){
+    storage.project = $scope.form.project;
+
+    console.log("project", $scope.form);
+    $window.location.reload(true);
+  }
+}]);
